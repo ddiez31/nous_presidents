38316ad68ff81b45babd5a4c5a4c4c831da6d609
David <david.diez31@free.fr>
2017-02-20 10:04:35 +0100

    initial

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    var game = new Phaser.Game(960, 639, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+
+    function preload() {
+
+
+    }
+
+    function create() {
+
+
+    }
+
+    function update() {
+
+
+    }
+
+
+
+
+});
